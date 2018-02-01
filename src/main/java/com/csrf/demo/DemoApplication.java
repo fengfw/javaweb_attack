125e37a5a4e2da219d3953887ef933593cac641c
fengfw <451488920@qq.com>
2018-02-01 17:33:28 +0800

    Update and rename Demo1Application.java to DemoApplication.java

--- src/main/java/com/csrf/demo/DemoApplication.java
+++ src/main/java/com/csrf/demo/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo1Application {
+public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);

